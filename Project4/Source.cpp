49462fa063586823455a9a19836e861160596e76
Shidkung <pitpi@DESKTOP-B29REMC>
2021-08-09 14:10:36 +0700

    float x, y,sum,determine,divide,multi;  scanf_s("%f %f",&x,&y);  multi = x * y;  determine = x - y;  sum = x + y;  divide = x / y;  printf("%f\n",sum);  printf("%f\n", determine);  printf("%f\n", multi);  printf("%f\n",divide);

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main() {
-
-
-
-
+	float x, y,sum,determine,divide,multi;
+	scanf_s("%f %f",&x,&y);
+	multi = x * y;
+	determine = x - y;
+	sum = x + y;
+	divide = x / y;
+	printf("%f\n",sum);
+	printf("%f\n", determine);
+	printf("%f\n", multi);
+	printf("%f\n",divide);
 return 0;
 }
